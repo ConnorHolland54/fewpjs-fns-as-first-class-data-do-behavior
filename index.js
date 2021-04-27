b6e7df77a970437f3222bdf7737abe16f4b86334
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-27 13:56:34 -0600

    commit tree e542364ab561030d0a2d0d311d8afb6dbbdddeb2

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  return displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let num = parseInt(string.split(':')[0])
+  if (num < 12 ) {
+    return "Good Morning"
+  } else if (num >= 12 && num < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
